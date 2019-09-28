b8f65b5d8d724fb42f6cd7c1c128b48c132d79aa
senya_1997 <senya_1997@mail.ru>
2019-09-28 13:36:43 +0300

    add V-USB lib, dev main

---
+++ MIDI_keyboard/MIDI_keyboard/defines.h
@@ -0,0 +1,7 @@
+#define F_CPU 12000000L
+
+#define USB_PORT PORTD
+#define USB_DDR DDRD
+
+#define BUT_PORT PORTB
+#define BUT_DDR DDRB
--- MIDI_keyboard/MIDI_keyboard/main.c
+++ MIDI_keyboard/MIDI_keyboard/main.c
@@ -1,12 +1,36 @@
-#include <avr/io.h>
+#include "defines.h"
 
+#include <avr/io.h>
+#include <avr/interrupt.h>
+#include <util/delay.h>
+
+#include "usbdrv/usbdrv.h"
+#include "usbdrv/usbconfig.h"
+
+USB_PUBLIC uchar usbFunctionSetup(uchar data[8])
+{
+	return 0;
+}
 
 int main(void)
 {
+	USB_PORT &= ~(1 << USB_CFG_DMINUS_BIT) & ~(1 << USB_CFG_DPLUS_BIT);
+	USB_DDR &= ~(1 << USB_CFG_DMINUS_BIT) & ~(1 << USB_CFG_DPLUS_BIT);
 	
-    while (1) 
+	BUT_PORT = 0xFF;
+	BUT_DDR = 0x00;
+	
+	usbDeviceDisconnect();
+	_delay_ms(500);
+	usbDeviceConnect();
+	
+	usbInit();
+    
+	sei();
+	while (1) 
     {
-		
+		usbPoll();
+		_delay_ms(10);
     }
 }
 
