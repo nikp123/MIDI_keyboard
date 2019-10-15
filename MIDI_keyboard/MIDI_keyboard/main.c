ba4e5eade1c09b45c0045e8806f2cc788bec223c
senya_1997 <senya_1997@mail.ru>
2019-10-15 12:43:12 +0300

    add descriptor MIDI, recognized as audio device

--- MIDI_keyboard/MIDI_keyboard/main.c
+++ MIDI_keyboard/MIDI_keyboard/main.c
@@ -1,22 +1,226 @@
 #include "defines.h"
 
+#include <string.h>
+
 #include <avr/io.h>
 #include <avr/interrupt.h>
-#include <util/delay.h>
+#include <avr/pgmspace.h>
 
 #include "usbdrv/usbdrv.h"
-#include "usbdrv/usbconfig.h"
+//#include "usbdrv/usbconfig.h"
+
+const static char PROGMEM deviceDescrMIDI[] = {
+	0x12,					/* sizeof(usbDescriptorDevice): length of descriptor in bytes */
+	0x01,					/* descriptor type */
+	0x10, 0x01,				/* USB version supported */
+	0,						/* device class: defined at interface level */
+	0,						/* subclass */
+	0,						/* protocol */
+	8,						/* max packet size */
+	USB_CFG_VENDOR_ID,		/* 2 bytes */
+	USB_CFG_DEVICE_ID,		/* 2 bytes */
+	USB_CFG_DEVICE_VERSION,	/* 2 bytes */
+	0x01,					/* manufacturer string index */
+	0x02,					/* product string index */
+	0,						/* serial number string index */
+	0x01,					/* number of configurations */
+};
+
+const static char PROGMEM configDescrMIDI[] = {	/* USB configuration descriptor */
+	0x09,						/* sizeof(usbDescrConfig): length of descriptor in bytes */
+	USBDESCR_CONFIG,			/* descriptor type */
+	0x65, 0x00,					/* total length of data returned (including inlined descriptors) */
+	0x02,						/* number of interfaces in this configuration */
+	0x01,						/* index of this configuration */
+	0x00,						/* configuration name string index */
+	USBATTR_BUSPOWER,			/* attributes */
+	USB_CFG_MAX_BUS_POWER / 2,	/* max USB current in 2mA units */
+	
+	
+	
+	0x09,				/* sizeof(usbDescrInterface): length of descriptor in bytes */
+	USBDESCR_INTERFACE,	/* descriptor type */
+	0,					/* index of this interface */
+	0,					/* alternate setting for this interface */
+	0,					/* endpoints excl 0: number of endpoint descriptors to follow */
+	0x01,
+	0x01,
+	0,
+	0,					/* string index for interface */
+
+
+
+	0x09,		/* sizeof(usbDescrCDC_HeaderFn): length of descriptor in bytes */
+	0x24,		/* descriptor type */
+	0x01,		/* header functional descriptor */
+	0x00, 0x01,	/* bcdADC */
+	0x09, 0x00,	/* wTotalLength */
+	0x01,
+	0x01,
+
+
+
+	0x09,				/* length of descriptor in bytes */
+	USBDESCR_INTERFACE,	/* descriptor type */
+	0x01,				/* index of this interface */
+	0,					/* alternate setting for this interface */
+	0x02,				/* endpoints excl 0: number of endpoint descriptors to follow */
+	0x01,				/* AUDIO */
+	0x03,				/* MS */
+	0,					/* unused */
+	0,					/* string index for interface */
+
+
+
+	0x07,		/* length of descriptor in bytes */
+	0x24,		/* descriptor type */
+	0x01,		/* header functional descriptor */
+	0x00, 0x01,	/* bcdADC */
+	0x41, 0x00,	/* wTotalLength */
+
+
+
+	6,			/* bLength */
+	36,			/* descriptor type */
+	2,			/* MIDI_IN_JACK desc subtype */
+	1,			/* EMBEDDED bJackType */
+	1,			/* bJackID */
+	0,			/* iJack */
+
+	6,			/* bLength */
+	36,			/* descriptor type */
+	2,			/* MIDI_IN_JACK desc subtype */
+	2,			/* EXTERNAL bJackType */
+	2,			/* bJackID */
+	0,			/* iJack */
+
+	//B.4.4 MIDI OUT Jack Descriptor
+	9,			/* length of descriptor in bytes */
+	36,			/* descriptor type */
+	3,			/* MIDI_OUT_JACK descriptor */
+	1,			/* EMBEDDED bJackType */
+	3,			/* bJackID */
+	1,			/* No of input pins */
+	2,			/* BaSourceID */
+	1,			/* BaSourcePin */
+	0,			/* iJack */
+
+	9,			/* bLength of descriptor in bytes */
+	36,			/* bDescriptorType */
+	3,			/* MIDI_OUT_JACK bDescriptorSubtype */
+	2,			/* EXTERNAL bJackType */
+	4,			/* bJackID */
+	1,			/* bNrInputPins */
+	1,			/* baSourceID (0) */
+	1,			/* baSourcePin (0) */
+	0,			/* iJack */
+
+
+	// B.5 Bulk OUT Endpoint Descriptors
+
+	//B.5.1 Standard Bulk OUT Endpoint Descriptor
+	9,			/* bLenght */
+	USBDESCR_ENDPOINT,	/* bDescriptorType = endpoint */
+	0x1,			/* bEndpointAddress OUT endpoint number 1 */
+	3,			/* bmAttributes: 2:Bulk, 3:Interrupt endpoint */
+	8, 0,			/* wMaxPacketSize */
+	10,			/* bIntervall in ms */
+	0,			/* bRefresh */
+	0,			/* bSyncAddress */
+
+	// B.5.2 Class-specific MS Bulk OUT Endpoint Descriptor
+	5,			/* bLength of descriptor in bytes */
+	37,			/* bDescriptorType */
+	1,			/* bDescriptorSubtype */
+	1,			/* bNumEmbMIDIJack  */
+	1,			/* baAssocJackID (0) */
+
+
+	//B.6 Bulk IN Endpoint Descriptors
+
+	//B.6.1 Standard Bulk IN Endpoint Descriptor
+	9,			/* bLenght */
+	USBDESCR_ENDPOINT,	/* bDescriptorType = endpoint */
+	0x81,			/* bEndpointAddress IN endpoint number 1 */
+	3,			/* bmAttributes: 2: Bulk, 3: Interrupt endpoint */
+	8, 0,			/* wMaxPacketSize */
+	10,			/* bIntervall in ms */
+	0,			/* bRefresh */
+	0,			/* bSyncAddress */
+
+	// B.6.2 Class-specific MS Bulk IN Endpoint Descriptor
+	5,			/* bLength of descriptor in bytes */
+	37,			/* bDescriptorType */
+	1,			/* bDescriptorSubtype */
+	1,			/* bNumEmbMIDIJack (0) */
+	3,			/* baAssocJackID (0) */
+};
+
+uchar usbFunctionDescriptor(usbRequest_t * rq)
+{
+
+	if (rq->wValue.bytes[1] == USBDESCR_DEVICE) {
+		usbMsgPtr = (uchar *) deviceDescrMIDI;
+		return sizeof(deviceDescrMIDI);
+		} else {		/* must be config descriptor */
+		usbMsgPtr = (uchar *) configDescrMIDI;
+		return sizeof(configDescrMIDI);
+	}
+}
+
+static uchar sendEmptyFrame;
 
 USB_PUBLIC uchar usbFunctionSetup(uchar data[8])
 {
-	if(data[1] == USB_LED_ON) PORTD |= (1 << LED_D0) | (1 << LED_D1);
-	else PORTD &= ~(1 << LED_D0) & ~(1 << LED_D1);
-	
-	return 0;
+	usbRequest_t *rq = (void *) data;
+
+	// DEBUG LED
+	PORTC ^= (1 << LED_D0) ^ (1 << LED_D1);
+
+	if ((rq->bmRequestType & USBRQ_TYPE_MASK) == USBRQ_TYPE_CLASS) {	/* class request type */
+
+		/*  Prepare bulk-in endpoint to respond to early termination   */
+		if ((rq->bmRequestType & USBRQ_DIR_MASK) ==
+		USBRQ_DIR_HOST_TO_DEVICE)
+		sendEmptyFrame = 1;
+	}
+
+	return 0xff;
+}
+
+uchar usbFunctionRead(uchar * data, uchar len)
+{
+	// DEBUG LED
+	PORTD ^= (1 << LED_D0) ^ (1 << LED_D1);
+
+	data[0] = 0;
+	data[1] = 0;
+	data[2] = 0;
+	data[3] = 0;
+	data[4] = 0;
+	data[5] = 0;
+	data[6] = 0;
+
+	return 7;
+}
+
+uchar usbFunctionWrite(uchar * data, uchar len)
+{
+	// DEBUG LED
+	PORTD ^= (1 << LED_D0) ^ (1 << LED_D1);
+	return 1;
+}
+
+void usbFunctionWriteOut(uchar * data, uchar len)
+{
+	// DEBUG LED
+	PORTD ^= (1 << LED_D0) ^ (1 << LED_D1);
 }
 
 void main(void)
 {
+	volatile int i;
+	
 	PORTB |= (1 << BUT_B0) | (1 << BUT_B1) | (1 << BUT_B2) | (1 << BUT_B3);
 	DDRB &= ~(1 << BUT_B0) & ~(1 << BUT_B1) & ~(1 << BUT_B2) & ~(1 << BUT_B3);
 	
@@ -30,17 +234,18 @@ void main(void)
 	DDRD &= ~(1 << USB_CFG_DMINUS_BIT) & ~(1 << USB_CFG_DPLUS_BIT) & ~(1 << BUT_D0) & ~(1 << BUT_D1);	
 	
 	//usbDeviceDisconnect();
-	//_delay_ms(100);
-	usbDeviceConnect();
+	//for(i = 0; i <= )
 	
+	usbDeviceConnect();
 	usbInit();
     
 	sei();
 	while (1) 
     {
 		usbPoll();
-		_delay_ms(10);
 		
+		for (i = 0; i <= 32766; i++) // ~ 3 ms
+		{ }
     }
 }
 
--- MIDI_keyboard/MIDI_keyboard/usbdrv/usbconfig.h
+++ MIDI_keyboard/MIDI_keyboard/usbdrv/usbconfig.h
@@ -44,7 +44,7 @@ section at the end of this file).
  * interrupt, the USB interrupt will also be triggered at Start-Of-Frame
  * markers every millisecond.]
  */
-#define USB_CFG_CLOCK_KHZ       (F_CPU/1000)
+			#define USB_CFG_CLOCK_KHZ       (F_CPU/1000)
 /* Clock rate of the AVR in kHz. Legal values are 12000, 12800, 15000, 16000,
  * 16500, 18000 and 20000. The 12.8 MHz and 16.5 MHz versions of the code
  * require no crystal, they tolerate +/- 1% deviation from the nominal
@@ -53,7 +53,7 @@ section at the end of this file).
  * Since F_CPU should be defined to your actual clock rate anyway, you should
  * not need to modify this setting.
  */
-#define USB_CFG_CHECK_CRC       0
+			#define USB_CFG_CHECK_CRC       0
 /* Define this to 1 if you want that the driver checks integrity of incoming
  * data packets (CRC checks). CRC checks cost quite a bit of code size and are
  * currently only available for 18 MHz crystal clock. You must choose
@@ -76,7 +76,7 @@ section at the end of this file).
 
 /* --------------------------- Functional Range ---------------------------- */
 
-#define USB_CFG_HAVE_INTRIN_ENDPOINT    0
+#define USB_CFG_HAVE_INTRIN_ENDPOINT    1
 /* Define this to 1 if you want to compile a version with two endpoints: The
  * default control endpoint 0 and an interrupt-in endpoint (any other endpoint
  * number).
@@ -87,7 +87,7 @@ section at the end of this file).
  * configured below) and a catch-all default interrupt-in endpoint as above.
  * You must also define USB_CFG_HAVE_INTRIN_ENDPOINT to 1 for this feature.
  */
-#define USB_CFG_EP3_NUMBER              3
+			#define USB_CFG_EP3_NUMBER              3
 /* If the so-called endpoint 3 is used, it can now be configured to any other
  * endpoint number (except 0) with this macro. Default if undefined is 3.
  */
@@ -97,13 +97,13 @@ section at the end of this file).
  * Since the token is toggled BEFORE sending any data, the first packet is
  * sent with the oposite value of this configuration!
  */
-#define USB_CFG_IMPLEMENT_HALT          0
+#define USB_CFG_IMPLEMENT_HALT          1
 /* Define this to 1 if you also want to implement the ENDPOINT_HALT feature
  * for endpoint 1 (interrupt endpoint). Although you may not need this feature,
  * it is required by the standard. We have made it a config option because it
  * bloats the code considerably.
  */
-#define USB_CFG_SUPPRESS_INTR_CODE      0
+			#define USB_CFG_SUPPRESS_INTR_CODE      0
 /* Define this to 1 if you want to declare interrupt-in endpoints, but don't
  * want to send any data over them. If this macro is defined to 1, functions
  * usbSetInterrupt() and usbSetInterrupt3() are omitted. This is useful if
@@ -125,18 +125,18 @@ section at the end of this file).
  * The value is in milliamperes. [It will be divided by two since USB
  * communicates power requirements in units of 2 mA.]
  */
-#define USB_CFG_IMPLEMENT_FN_WRITE      0
+#define USB_CFG_IMPLEMENT_FN_WRITE      1
 /* Set this to 1 if you want usbFunctionWrite() to be called for control-out
  * transfers. Set it to 0 if you don't need it and want to save a couple of
  * bytes.
  */
-#define USB_CFG_IMPLEMENT_FN_READ       0
+#define USB_CFG_IMPLEMENT_FN_READ       1
 /* Set this to 1 if you need to send control replies which are generated
  * "on the fly" when usbFunctionRead() is called. If you only want to send
  * data from a static buffer, set it to 0 and return the data from
  * usbFunctionSetup(). This saves a couple of bytes.
  */
-#define USB_CFG_IMPLEMENT_FN_WRITEOUT   0
+#define USB_CFG_IMPLEMENT_FN_WRITEOUT   1
 /* Define this to 1 if you want to use interrupt-out (or bulk out) endpoints.
  * You must implement the function usbFunctionWriteOut() which receives all
  * interrupt/bulk data sent to any endpoint other than 0. The endpoint number
@@ -147,12 +147,12 @@ section at the end of this file).
  * of the macros usbDisableAllRequests() and usbEnableAllRequests() in
  * usbdrv.h.
  */
-#define USB_CFG_DRIVER_FLASH_PAGE       0
+			#define USB_CFG_DRIVER_FLASH_PAGE       0
 /* If the device has more than 64 kBytes of flash, define this to the 64 k page
  * where the driver's constants (descriptors) are located. Or in other words:
  * Define this to 1 for boot loaders on the ATMega128.
  */
-#define USB_CFG_LONG_TRANSFERS          0
+			#define USB_CFG_LONG_TRANSFERS          0
 /* Define this to 1 if you want to send/receive blocks of more than 254 bytes
  * in a single control-in or control-out transfer. Note that the capability
  * for long transfers increases the driver size.
@@ -173,7 +173,7 @@ section at the end of this file).
 /* This macro (if defined) is executed when a USB SET_ADDRESS request was
  * received.
  */
-#define USB_COUNT_SOF                   0
+			#define USB_COUNT_SOF                   0
 /* define this macro to 1 if you need the global variable "usbSofCount" which
  * counts SOF packets. This feature requires that the hardware interrupt is
  * connected to D- instead of D+.
@@ -197,7 +197,7 @@ section at the end of this file).
  * Please note that Start Of Frame detection works only if D- is wired to the
  * interrupt, not D+. THIS IS DIFFERENT THAN MOST EXAMPLES!
  */
-#define USB_CFG_CHECK_DATA_TOGGLING     0
+			#define USB_CFG_CHECK_DATA_TOGGLING     0
 /* define this macro to 1 if you want to filter out duplicate data packets
  * sent by the host. Duplicates occur only as a consequence of communication
  * errors, when the host does not receive an ACK. Please note that you need to
@@ -205,11 +205,11 @@ section at the end of this file).
  * usbFunctionWrite(). Use the global usbCurrentDataToken and a static variable
  * for each control- and out-endpoint to check for duplicate packets.
  */
-#define USB_CFG_HAVE_MEASURE_FRAME_LENGTH   0
+			#define USB_CFG_HAVE_MEASURE_FRAME_LENGTH   0
 /* define this macro to 1 if you want the function usbMeasureFrameLength()
  * compiled in. This function can be used to calibrate the AVR's RC oscillator.
  */
-#define USB_USE_FAST_CRC                0
+			#define USB_USE_FAST_CRC                0
 /* The assembler module has two implementations for the CRC algorithm. One is
  * faster, the other is smaller. This CRC routine is only used for transmitted
  * messages where timing is not critical. The faster routine needs 31 cycles
@@ -229,7 +229,7 @@ section at the end of this file).
  * with libusb: 0x16c0/0x5dc.  Use this VID/PID pair ONLY if you understand
  * the implications!
  */
-#define  USB_CFG_DEVICE_ID       0xdc, 0x05 /* = 0x05dc = 1500 */
+#define  USB_CFG_DEVICE_ID       0xe4, 0x05 /* = 0x05e4 = 1508 */
 /* This is the ID of the product, low byte first. It is interpreted in the
  * scope of the vendor ID. If you have registered your own VID with usb.org
  * or if you have licensed a PID from somebody else, define it here. Otherwise
@@ -240,7 +240,7 @@ section at the end of this file).
  * with libusb: 0x16c0/0x5dc.  Use this VID/PID pair ONLY if you understand
  * the implications!
  */
-#define USB_CFG_DEVICE_VERSION  0x00, 0x01
+#define USB_CFG_DEVICE_VERSION  0x01, 0x00
 /* Version number of the device: Minor number first, then major number.
  */
 #define USB_CFG_VENDOR_NAME     'S', 'S'
@@ -268,20 +268,20 @@ section at the end of this file).
  * to fine tune control over USB descriptors such as the string descriptor
  * for the serial number.
  */
-#define USB_CFG_DEVICE_CLASS        0xff    /* set to 0 if deferred to interface */
+#define USB_CFG_DEVICE_CLASS        0    /* set to 0 if deferred to interface */
 #define USB_CFG_DEVICE_SUBCLASS     0
 /* See USB specification if you want to conform to an existing device class.
  * Class 0xff is "vendor specific".
  */
-#define USB_CFG_INTERFACE_CLASS     0   /* define class here if not at device level */
-#define USB_CFG_INTERFACE_SUBCLASS  0
+#define USB_CFG_INTERFACE_CLASS     1   /* define class here if not at device level */
+#define USB_CFG_INTERFACE_SUBCLASS  3
 #define USB_CFG_INTERFACE_PROTOCOL  0
 /* See USB specification if you want to conform to an existing device class or
  * protocol. The following classes must be set at interface level:
  * HID class is 3, no subclass and protocol required (but may be useful!)
  * CDC class is 2, use subclass 2 and protocol 1 for ACM
  */
-/* #define USB_CFG_HID_REPORT_DESCRIPTOR_LENGTH    42 */
+#define USB_CFG_HID_REPORT_DESCRIPTOR_LENGTH    0  /* total length of report descriptor */
 /* Define this to the length of the HID report descriptor, if you implement
  * an HID device. Otherwise don't define it or define it to 0.
  * If you use this define, you must add a PROGMEM character array named
@@ -346,14 +346,14 @@ section at the end of this file).
  * };
  */
 
-#define USB_CFG_DESCR_PROPS_DEVICE                  0
-#define USB_CFG_DESCR_PROPS_CONFIGURATION           0
+#define USB_CFG_DESCR_PROPS_DEVICE                  USB_PROP_IS_DYNAMIC
+#define USB_CFG_DESCR_PROPS_CONFIGURATION           USB_PROP_IS_DYNAMIC
 #define USB_CFG_DESCR_PROPS_STRINGS                 0
 #define USB_CFG_DESCR_PROPS_STRING_0                0
 #define USB_CFG_DESCR_PROPS_STRING_VENDOR           0
 #define USB_CFG_DESCR_PROPS_STRING_PRODUCT          0
 #define USB_CFG_DESCR_PROPS_STRING_SERIAL_NUMBER    0
-#define USB_CFG_DESCR_PROPS_HID                     0
+#define USB_CFG_DESCR_PROPS_HID                     0	// USB_PROP_IS_DYNAMIC
 #define USB_CFG_DESCR_PROPS_HID_REPORT              0
 #define USB_CFG_DESCR_PROPS_UNKNOWN                 0
 
