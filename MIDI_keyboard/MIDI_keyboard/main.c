5b02ab8caafb2b28e2a374ae4358c39c1221dfda
senya_1997 <senya_1997@mail.ru>
2019-10-21 01:46:20 +0300

    add handling 8 buttons (need to check)

--- MIDI_keyboard/MIDI_keyboard/main.c
+++ MIDI_keyboard/MIDI_keyboard/main.c
@@ -10,6 +10,15 @@
 #include "usbdrv/usbdrv.h"
 #include "descriptors.h"
 
+#define KEY_STATUS ((PINB & 0b00001111) | (PINC & 0b00110000) | (PIND & 0b11000000))
+//   key num: 7 6 5 4 3 2 1 0
+// PORT name: D D C C B B B B, bind by scheme
+/* equivalent to this:		  
+		(PINB & ((1 << BUT_B0) | (1 << BUT_B1) | (1 << BUT_B2) | (1 << BUT_B3))) | 
+		(PINC & ((1 << BUT_C0) | (1 << BUT_C1))) | 
+		(PIND & ((1 << BUT_D0) | (1 << BUT_D1)))	
+*/
+
 uchar curInt = 0x01;
 uchar bReq;
 uchar wInd;
@@ -68,17 +77,6 @@ USB_PUBLIC uchar usbFunctionSetup(uchar data[8])
 			else return USB_NO_MSG;
 		default: return USB_NO_MSG;
 	}
-
-/*
-	if ((rq->bmRequestType & USBRQ_TYPE_MASK) == USBRQ_TYPE_CLASS) // class request type 
-	{	
-
-		//  Prepare bulk-in endpoint to respond to early termination  
-		
-		if ((rq->bmRequestType & USBRQ_DIR_MASK) == USBRQ_DIR_HOST_TO_DEVICE)
-			sendEmptyFrame = 1;
-	}
-*/
 }
 
 uchar usbFunctionRead(uchar * data, uchar len)
@@ -123,8 +121,27 @@ void usbFunctionWriteOut(uchar * data, uchar len)
 
 void main(void)
 {
+	const uchar keyCode [8] = 
+	{		// PIN-PORT -> "note"
+		0x3C, //   0-B -> "C" middle octave: key code "0"
+		0x3E, //   1-B -> "D"
+		0x40, //   2-B -> "E"
+		0x41, //   3-B -> "F"
+		0x43, //   4-C -> "G"
+		0x45, //   5-C -> "A"
+		0x47, //   6-D -> "H"
+		0x48  //   7-D -> "C"
+	};
+	
+	uchar i;
 	uchar midiMsg[4];
-	uchar but;
+	
+	uchar key[3]; // keys status: for "majoritar"
+	uchar key_old = 0xFF; 
+	uchar key_mask;
+	
+	uchar key_num_ch[8] = // number of keys that was changed: release (2) or press (1), don't changed (0)
+							{ 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, };
 	
 	PORTB |= (1 << BUT_B0) | (1 << BUT_B1) | (1 << BUT_B2) | (1 << BUT_B3);
 	DDRB &= ~(1 << BUT_B0) & ~(1 << BUT_B1) & ~(1 << BUT_B2) & ~(1 << BUT_B3);
@@ -136,10 +153,7 @@ void main(void)
 	PORTD |= (1 << BUT_D0) | (1 << BUT_D1);
 	
 	DDRD |= (1 << LED_D0) | (1 << LED_D1);
-	DDRD &= ~(1 << USB_CFG_DMINUS_BIT) & ~(1 << USB_CFG_DPLUS_BIT) & ~(1 << BUT_D0) & ~(1 << BUT_D1);	
-	
-	//usbDeviceDisconnect();
-	//for(i = 0; i <= )
+	DDRD &= ~(1 << USB_CFG_DMINUS_BIT) & ~(1 << USB_CFG_DPLUS_BIT) & ~(1 << BUT_D0) & ~(1 << BUT_D1);
 	
 	usbDeviceConnect();
 	usbInit();
@@ -149,25 +163,57 @@ void main(void)
     {
 		usbPoll();
 		
-		if(usbInterruptIsReady())
-		{
-			but = PINB;
-			if((but & 0x01) == 0x00)
+		// keys polling:
+			key[0] = KEY_STATUS;
+				_delay_ms(2);
+			key[1] = KEY_STATUS;
+				_delay_ms(2);
+			key[2] = KEY_STATUS;
+		
+		// send MIDI msg if keys
+		if(usbInterruptIsReady() & 
+			(key[0] == key[1]) & (key[1] == key[2]) & 
+			(key[0] != key_old))
+		{	  
+			key_mask = key[0] ^ key_old;
+			
+			key[0] &= key_mask;
+			key_old &= key_mask;
+			
+			for(i = 0; i <= 7; i++)
 			{
-				midiMsg[0] = 0x09;
-				midiMsg[1] = 0x90; // channel for "Note on" event: 0b1001_NNNN, NNNN - number of channel
-				midiMsg[2] = 0x3C; // code note
-				midiMsg[3] = 0x7F; // velocity
-				PORTD ^= (1 << LED_D1);
-			}
-			else
-			{
-				midiMsg[0] = 0x08;
-				midiMsg[1] = 0x80; // "Note off" event: 0b1000_NNNN
-				midiMsg[2] = 0x3C;
-				midiMsg[3] = 0x00;
+				if(((key_old & 0x01) - (key[0] & 0x01)) == 1) key_num_ch[i] = 1; // key was pressed
+				else if(((key_old & 0x01) - (key[0] & 0x01)) == 255) key_num_ch[i] = 2; // released
 				
+				key_mask >>= 1;
+				key[0] >>= 1;
+				key_old >>= 1;
 			}
+			
+			for(i = 0; i <= 7; i++)
+			{
+				if(key_num_ch[i] == 1)
+				{
+					midiMsg[0] = 0x09;
+					midiMsg[1] = 0x90;			// channel for "Note on" event: 0b1001_NNNN, NNNN - number of channel
+					midiMsg[2] = keyCode[i];	// code note
+					midiMsg[3] = 0x7F;			// velocity
+					
+					key_num_ch[i] = 0;
+					PORTD ^= (1 << LED_D1);
+				}
+				else if(key_num_ch[i] == 2)
+				{
+					midiMsg[0] = 0x08;
+					midiMsg[1] = 0x80;			// "Note off" event: 0b1000_NNNN
+					midiMsg[2] = keyCode[i];
+					midiMsg[3] = 0x00;
+					
+					key_num_ch[i] = 0;
+				}
+			}
+			
+			key_old = key[1];
 			usbSetInterrupt(midiMsg, 4);
 		}
 		
@@ -177,8 +223,6 @@ ERROR:	if(curInt == 0x00)
 			usbPoll();
 			goto ERROR;
 		}
-		
-		_delay_ms(3);
     }
 }
 
